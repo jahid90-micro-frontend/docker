51942fe205558e84b50be1860c166de778f2203d
Jahid <jahid90@users.noreply.github.com>
2021-05-21 18:32:11 +0530

    update entries

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "Setting up the workspace..."
 
 git clone git@github.com:jahid90-micro-frontend/gateway.git gateway
+git clone git@github.com:jahid90-micro-frontend/web-service.git web-service
 git clone git@github.com:jahid90-micro-frontend/page-service.git page-service
 git clone git@github.com:jahid90-micro-frontend/layout-service.git layout-service
 git clone git@github.com:jahid90-micro-frontend/content-service.git content-service
@@ -14,5 +15,7 @@ cd widgets
 git clone git@github.com:jahid90-micro-frontend/nav-widget.git nav
 git clone git@github.com:jahid90-micro-frontend/footer-widget.git footer
 git clone git@github.com:jahid90-micro-frontend/home-widget.git home
+git clone git@github.com:jahid90-micro-frontend/about-widget.git about
+git clone git@github.com:jahid90-micro-frontend/not-found-widget.git not-found
 
 echo "Done!"
